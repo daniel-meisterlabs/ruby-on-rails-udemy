2fa8f63152d36fa1326ad7a1e0bbd732af091a38
Daniel Rötzer <52409858+daniel-meisterlabs@users.noreply.github.com>
2021-07-07 11:26:34 +0200

    add /about page

--- first-rails-app/config/routes.rb
+++ first-rails-app/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # Points to 'app/views/pages/home.html.erb'
   root 'pages#home'
+
+  # Defines the GET /about route and where it points to
+  get 'about', to: 'pages#about'
 end
